9183b62c10cfc11d93acd79b719fcfdab24cfa47
Martin Georgiev <78639036+Martin-Georgiev-21@users.noreply.github.com>
2023-03-18 11:47:24 +0200

    FileStructure

---
+++ Hotel-Reservation-Menager/Hotel-Reservation-Menager/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList_WebAppDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Hotel-Reservation-Menager/Hotel-Reservation-Menager/Models/Clients.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Reservation_Menager.Models
+{
+    public class Clients
+    {
+    }
+}
