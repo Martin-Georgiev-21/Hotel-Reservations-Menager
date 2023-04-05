f66dcaf4153ee9b813f37f0ea426506702c0988e
Martin Georgiev <78639036+Martin-Georgiev-21@users.noreply.github.com>
2023-04-05 16:10:37 +0300

    Sorting For Users
    Юзърите се сортират по Username, First Name, Middle Name, Last Name и Email;
    Когато се сортират по едно от полетата и се премине към друга страница сортировката се запазва.
    При преминаването към странициране се сортира автоматично по ID

--- Hotel-Reservation-Menager/Hotel-Reservation-Menager/Controllers/UsersController.cs
+++ Hotel-Reservation-Menager/Hotel-Reservation-Menager/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Hotel_Reservation_Menager.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,18 +18,69 @@ namespace Hotel_Reservation_Menager.Controllers
         {
             _db = db;
         }
-        public IActionResult Index(int page = 1, int pageSize = 6)
+        public IActionResult Index(string sortOrder, int clicked = 0, int page = 1, int pageSize = 6)
         {
-            var data = _db.Users.ToList();
-            var totalCount = data.Count();
-            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            var users = from s in _db.Users
+                        select s;
 
-            var pageData = data.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var totalCount = users.Count();
+            var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+            var currentclicked = clicked;
+
+            switch (sortOrder, clicked)
+            {
+                case ("FirstName", 1):
+                    users = users.OrderBy(s => s.FirstName);
+                    clicked = 0;
+                    break;
+                case ("FirstName", 0):
+                    users = users.OrderByDescending(s => s.FirstName);
+                    clicked = 1;
+                    break;
+                case ("MiddleName", 1):
+                    users = users.OrderBy(s => s.MiddleName);
+                    clicked = 0;
+                    break;
+                case ("MiddleName", 0):
+                    users = users.OrderByDescending(s => s.MiddleName);
+                    clicked = 1;
+                    break;
+                case ("LastName", 1):
+                    users = users.OrderBy(s => s.LastName);
+                    clicked = 0;
+                    break;
+                case ("LastName", 0):
+                    users = users.OrderByDescending(s => s.LastName);
+                    clicked = 1;
+                    break;
+                case ("Username", 1):
+                    users = users.OrderBy(s => s.Username);
+                    clicked = 0;
+                    break;
+                case ("Username", 0):
+                    users = users.OrderByDescending(s => s.Username);
+                    clicked = 1;
+                    break;
+                case ("Email", 1):
+                    users = users.OrderBy(s => s.Email);
+                    break;
+                case ("Email", 0):
+                    users = users.OrderByDescending(s => s.Email);
+                    break;
+                default:
+                    users = users.OrderBy(s => s.UserId);
+                    break;
+            }
+
+            var pageData = users.Skip((page - 1) * pageSize).Take(pageSize).ToList();
 
             ViewBag.Page = page;
             ViewBag.PageSize = pageSize;
             ViewBag.TotalCount = totalCount;
             ViewBag.TotalPages = totalPages;
+            ViewBag.Clicked = clicked;
+            ViewBag.CurrentClicked = currentclicked;
+            ViewBag.SortOrder = sortOrder;
 
             return View(pageData);
         }
